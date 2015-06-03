61b1d4e620c62ca8c4caa08bc57f826bd2cb1ad6
Danny Burt <daniel.burt@workday.com>
2015-06-02 19:44:37 -0700

    started allowing users to log in by username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,12 +25,20 @@
 #  admin                  :boolean          default(FALSE)
 #
 
-class  User < ActiveRecord::Base
+class User < ActiveRecord::Base
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
 
+  def login=(login)
+    @login = login
+  end
 
-
+  def login
+    @login || self.username || self.email
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603003948) do
+ActiveRecord::Schema.define(version: 20150603021457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string   "username",                               null: false
-    t.string   "first_name",                             null: false
-    t.string   "last_name"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.string   "email",                  default: "",    null: false
