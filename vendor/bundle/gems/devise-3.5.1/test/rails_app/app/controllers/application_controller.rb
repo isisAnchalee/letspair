023f43dfbfce1a183598008605bc3dcdb31013d8
Danny Burt <daniel.burt@workday.com>
2015-06-03 20:15:39 -0700

    working auth

--- vendor/bundle/gems/devise-3.5.1/test/rails_app/app/controllers/application_controller.rb
+++ vendor/bundle/gems/devise-3.5.1/test/rails_app/app/controllers/application_controller.rb
@@ -3,21 +3,10 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   before_filter :current_user, unless: :devise_controller?
   before_filter :authenticate_user!, if: :devise_controller?
   respond_to *Mime::SET.map(&:to_sym)
 
   devise_group :commenter, contains: [:user, :admin]
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end
 end
 
