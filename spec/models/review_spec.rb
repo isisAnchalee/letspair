d0f33ee59c065caa85334b1414b1e42b64a1f5b3
Daniel <BurtDaniel@Gmail.com>
2015-06-10 00:01:36 -0700

    Added associations to review spec

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -16,7 +16,9 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
+  
   context "validation" do
+    
     it "properly validates a correct review" do
       review = Review.new(reviewer_id: 1, reviewed_id: 2, title: "Hello", body: "World", rating: 10, project_id: 1)
       expect(review).to be_valid
@@ -25,6 +27,27 @@ RSpec.describe Review, type: :model do
     it "properly validates an incorrect review" do
       review = Review.new(reviewer_id: 1, reviewed_id: 2, title: "Hello", body: "", rating: 10, project_id: 1)
       expect(review).to_not be_valid
+    end
+  end
+  
+  context "associations" do
+    
+    it "has a reviewer" do
+      reviewer = FactoryGirl.create(:user)
+      review = FactoryGirl.create(:review, reviewer_id: reviewer.id)
+      expect(review.reviewer).to eq(reviewer)
+    end
+    
+    it "has a reviewee" do
+      reviewed = FactoryGirl.create(:user)
+      review = FactoryGirl.create(:review, reviewed_id: reviewed.id)
+      expect(review.reviewed).to eq(reviewed)
+    end
+    
+    it "has a project" do
+      project = FactoryGirl.create(:project)
+      review = FactoryGirl.create(:review, project_id: project.id)
+      expect(review.project).to eq(project)
     end
   end
 end
