abc480a6c51c2e49211dc675096c5343476aba0e
Daniel <BurtDaniel@Gmail.com>
2015-06-16 20:38:30 -0700

    100% project coverage

--- app/models/project.rb
+++ app/models/project.rb
@@ -36,11 +36,15 @@ class Project < ActiveRecord::Base
   attr_accessor :price
 
   validates :title, :user_id, :description, :complexity, :price, presence: true
+  attr_accessor :price
+
+  validates :title, :user_id, :description, :complexity, presence: true
   validates :title, :description, length: { minimum: 2 }
   
   belongs_to :author, foreign_key: :user_id, class_name: "User"
   has_one :review, dependent: :destroy
   has_many :bids, dependent: :destroy
+
 
   def price_in_dollars
     self.price.to_f / 100
