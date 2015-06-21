ee74d8b5e1092997afa7f27d9064ee7487beb209
Daniel <BurtDaniel@Gmail.com>
2015-06-20 18:24:10 -0700

    added wait time differently

--- spec/features/registrations_controller_spec.rb
+++ spec/features/registrations_controller_spec.rb
@@ -3,23 +3,25 @@ require 'rails_helper'
 RSpec.feature "User Registration", :type => :feature do
 
   scenario 'User signs up with valid credentials' do
+    Capybara.default_wait_time = 15
     visit '/users/sign_up'
-    fill_in 'Username', with: 'dannnnneh', wait: 15
+    fill_in 'Username', with: 'dannnnneh'
     fill_in 'Email', with: 'test@example.com'
     fill_in 'Password', with: 'passwordpassword'
     fill_in 'Password confirmation', with: 'passwordpassword'
     click_button 'Sign up'
-    expect(page).to have_content 'Sign Out', wait: 15
+    expect(page).to have_content 'Sign Out'
   end
 
   scenario 'User signs up with invalid credentials' do
+    Capybara.default_wait_time = 15
     visit '/users/sign_up'
-    fill_in 'Username', with: '', wait: 15
+    fill_in 'Username', with: ''
     fill_in 'Email', with: 'test@co.m'
     fill_in 'Password', with: 'asswordpassword'
     fill_in 'Password confirmation', with: 'passwordpassword'
     click_button 'Sign up'
-    expect(page).to have_content "Password confirmation doesn't match Password", wait: 15
+    expect(page).to have_content "Password confirmation doesn't match Password"
     expect(page).to have_content "Username can't be blank"
   end
 end
--- spec/features/sessions_controller_spec.rb
+++ spec/features/sessions_controller_spec.rb
@@ -3,39 +3,43 @@ require 'rails_helper'
 RSpec.feature "Sessions Controller", :type => :feature do
 
   scenario 'User signs in with username' do
+    Capybara.default_wait_time = 15
     visit '/users/sign_in'
     user = FactoryGirl.create(:user, username: "danneh", password: "oneoneone")
-    fill_in 'Login', with: user.username, wait: 15
+    fill_in 'Login', with: user.username
     fill_in 'Password', with: user.password
     click_button 'Log in'
-    expect(page).to have_content 'Sign Out', wait: 15
+    expect(page).to have_content 'Sign Out'
   end
 
   scenario 'User signs in with email' do
+    Capybara.default_wait_time = 15
     visit '/users/sign_in'
     user = FactoryGirl.create(:user, email: "danneh@danneh.danneh", password: "oneoneone")
-    fill_in 'Login', with: user.email, wait: 15
+    fill_in 'Login', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
-    expect(page).to have_content 'Sign Out', wait: 15
+    expect(page).to have_content 'Sign Out'
   end
 
   scenario 'User signs in with invalid email' do
     # Todo: Error messages
+    Capybara.default_wait_time = 15
     visit '/users/sign_in'
-    fill_in 'Login', with: "me@me.com", wait: 15
+    fill_in 'Login', with: "me@me.com"
     fill_in 'Password', with: "password"
     click_button 'Log in'
-    expect(page).to have_content 'Log in', wait: 15
+    expect(page).to have_content 'Log in'
   end
 
   scenario 'User signs out' do
+    Capybara.default_wait_time = 15
     visit '/users/sign_in'
     user = FactoryGirl.create(:user, email: "danneh@danneh.danneh", password: "oneoneone")
-    fill_in 'Login', with: user.email, wait: 15
+    fill_in 'Login', with: user.email
     fill_in 'Password', with: user.password
     click_button 'Log in'
-    click_link 'Sign Out', wait: 15
-    expect(page).to have_content 'Sign In', wait: 15
+    click_link 'Sign Out'
+    expect(page).to have_content 'Sign In'
   end
 end
