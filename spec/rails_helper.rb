04309ffe585ba46b644f264290f2ce0e14298c18
Daniel <BurtDaniel@Gmail.com>
2015-06-25 23:23:27 -0700

    moved capybara settings from rails_helper.rb to spec_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,13 +3,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
-require 'capybara'
-
-require 'capybara/poltergeist'
-Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, timeout: 15)
-end
-Capybara.javascript_driver = :poltergeist
 
 
 # Add additional requires below this line. Rails is not loaded until this point!
