b29148567febcd2fb789e265f05b1b3c4c801d3d
isisanchalee <isis.wenger@onelogin.com>
2015-07-15 22:56:38 -0700

    Add follows controller

---
+++ app/controllers/api/follows_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  class FollowsController < ApiController
+    respond_to :json
+    
+    before_action :require_signed_in!
+
+    def create
+      user = User.find(params[:user_id])
+
+      @follow = Follow.new(
+        follower_id: current_user.id,
+        followee_id: user.id
+      )
+
+      if @follow.save
+        render json: @follow
+      else
+        render json: @follow.errors.full_messages, status: 422
+      end
+    end
+
+    def destroy
+      @follow = Follow.find_by(followee_id: params[:user_id], follower_id: current_user.id)
+      @follow.try(:destroy)
+      render json: { "follow" => false }
+    end 
+  end
+end
