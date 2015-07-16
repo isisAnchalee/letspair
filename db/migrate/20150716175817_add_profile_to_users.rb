1a1af190ca9fbe998224656e875ff5625aafd284
isisanchalee <isis.wenger@onelogin.com>
2015-07-16 11:06:23 -0700

    Add profile migration
    Fix migration

---
+++ db/migrate/20150716175817_add_profile_to_users.rb
@@ -0,0 +1,17 @@
+class AddProfileToUsers < ActiveRecord::Migration
+  def change
+    create_table(:profiles) do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.integer :user_id, null: false
+      t.string :short_tag_line
+      t.text :description
+      t.text :work_history
+      t.timestamps null:false
+    end
+
+    add_index :profiles, :user_id
+    add_index :profiles, :email
+  end
+end
