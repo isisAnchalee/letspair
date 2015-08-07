2b02802ae73da053fce6c878ef86197cf4c0b4ba
Daniel <BurtDaniel@Gmail.com>
2015-08-06 19:28:54 -0700

    fixed omni auth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,47 +49,65 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
          :authentication_keys => [:login],
          :omniauth_providers => [:facebook]
 
   validates_presence_of :first_name, :last_name, :email
+  validates_uniqueness_of :email, case_sensitive: false
 
   scope :consultants, -> { where(is_company: false) }
   scope :companies, -> { where(is_company: true) }
 
-  # Devise
-  def login
-    self.email
-  end
+  TEMP_EMAIL_PREFIX = 'change@me'
+  TEMP_EMAIL_REGEX = /\Achange@me/
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions.to_hash).where(["lower(email) = :value", { :value => login.downcase }]).first
-    else
-      conditions[:email].downcase! if conditions[:email]
-      where(conditions.to_hash).first
-    end
-  end
+  def self.find_for_oauth(auth, signed_in_resource = nil)
 
-  # Devise omni auth
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.username
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
+    # Get the identity and user if they exist
+    identity = Identity.find_for_oauth(auth)
 
-  # Devise omni auth
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+    # If a signed_in_resource is provided it always overrides the existing user
+    # to prevent the identity being locked with accidentally created accounts.
+    # Note that this may leave zombie accounts (with no associated identity) which
+    # can be cleaned up at a later date.
+    user = signed_in_resource ? signed_in_resource : identity.user
+
+    # Create the user if needed
+    if user.nil?
+
+      # Get the existing user by email if the provider gives us a verified email.
+      # If no verified email was provided we assign a temporary email and ask the
+      # user to verify it on the next step via UsersController.finish_signup
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      email = auth.info.email if email_is_verified
+      user = User.where(:email => email).first if email
+
+      # Create the user if it's a new registration
+      if user.nil?
+        user = User.new(
+            first_name: auth.extra.raw_info.first_name,
+            last_name: auth.extra.raw_info.last_name,
+            #username: auth.info.nickname || auth.uid,
+            email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+            password: Devise.friendly_token[0,20]
+        )
+        user.skip_confirmation!
+        user.save!
       end
     end
+
+    # Associate the identity with the user if needed
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user
+  end
+
+  def email_verified?
+    self.email && self.email !~ TEMP_EMAIL_REGEX
   end
 
   def profile
