85d7563b1463a595983a63c68850cbd0cd74a0b2
Daniel <BurtDaniel@Gmail.com>
2015-08-09 14:44:57 -0700

    Updated specs to 99%+ coverage

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -12,6 +12,10 @@
 
 require 'rails_helper'
 
+class Auth
+  attr_accessor :uid, :provider
+end
+
 RSpec.describe Identity, type: :model do
   context "validations" do
     it { should validate_presence_of :uid }
@@ -21,6 +25,28 @@ RSpec.describe Identity, type: :model do
 
   context "associations" do
     it { should belong_to :user }
+  end
+
+  context "devise" do
+    it "#find_for_oauth finds an identity" do
+      auth = Auth.new
+      auth.uid = 5
+      auth.provider = "me"
+      user = FactoryGirl.create(:user)
+      identity = FactoryGirl.create(:identity, user_id: user.id, uid: auth.uid, provider: auth.provider)
+
+      expect(Identity.find_for_oauth(auth)).to eq(identity)
+    end
+
+    it "#find_for_oauth creates an identity" do
+      auth = Auth.new
+      auth.uid = 5
+      auth.provider = "me"
+
+      expect(Identity.all.count).to eq(0)
+      Identity.find_for_oauth(auth)
+      expect(Identity.all.count).to eq(1)
+    end
   end
 
 end
--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -24,5 +24,26 @@ RSpec.describe UserProfile, type: :model do
     before { FactoryGirl.create(:user_profile) }
     it { should belong_to :user }
   end
+
+  context "methods" do
+    let!(:user) { FactoryGirl.create(:user) }
+    let!(:profile) { FactoryGirl.create(:user_profile, user_id: user.id) }
+
+    it "#first_name" do
+      expect(profile.first_name).to eq(user.first_name)
+    end
+
+    it "#last_name" do
+      expect(profile.last_name).to eq(user.last_name)
+    end
+
+    it "#full_name" do
+      expect(profile.full_name).to eq("#{user.first_name} #{user.last_name}")
+    end
+
+    it "#email" do
+      expect(profile.email).to eq(user.email)
+    end
+  end
 end
 
